68e72f78659601ea05dae28593c0f3b994309dee
Sumit <sumittiruwa913@gmail.com>
2026-08-23 15:30:46 +0545

    design

--- LLM/app.py
+++ LLM/app.py
@@ -1,7 +1,25 @@
 import streamlit as st
 
+st.set_page_config(layout="wide") # Set the layout to wide
+
 #title of our app 
 st.title("Ai blog")
 
 #subheader
-st.subheader("Generate your blog using AI")
+st.subheader("Generate your blog using AI") 
+
+#siderbar
+with st.sidebar:
+    st.title("input your prompt here")
+    st.subheader("enter the deatil here")
+    
+    
+    # blog Title 
+    blog_title = st.text_input("Enter your blog title")
+    
+    #keyoword
+    keyword = st.text_input("Enter your keyword (comma-seperated)")
+    
+    
+    # Number of Words
+    num_words = st.slider("Select the number of words for your blog", min_value=100, max_value=2000, value=500, step=50)
